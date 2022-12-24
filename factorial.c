ccbdbcff6ae8e6acdb802f91de7c15fb96ea4e75
PrashanthReddy02 <120360128+PrashanthReddy02@users.noreply.github.com>
2022-12-24 16:46:12 +0530

    factorial.c updated

--- factorial.c
+++ factorial.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-int fact(int);
+int fact(int);//function declarion
 int main()
 {
     int n, res;
     printf("Enter the number:\n");
-    scanf("%d", &n);
+    scanf("%d", &n);//reads the number from the user
     res = fact(n);
     printf("the factorial of %d is %d\n", n, res);
     return 0;
 }
-int fact(int n)
+int fact(int n)//function for factorial
 {
     if (n == 0 || n == 1)
         return 1;
